9c758414a1e4769f7605f0b8da6401c065925160
Kevin German <kevger123@gmail.com>
2020-07-02 11:55:04 +0200

    updated explanation

--- src/components/ExplanationWhy.vue
+++ src/components/ExplanationWhy.vue
@@ -11,31 +11,46 @@
           <v-card-text>
             <p>
               Narrative Economics is the study of how narratives convey values
-              and emotions, how they spread and thus influence the economy. In
-              order to understand recessions, for example, it is not enough to
-              understand the cybernetic control loops behind them, but one must
-              also include the human factor with its irrational, emotionally
-              driven actions. Above all, this includes his belief in stories
-              that create meaning, the narratives. In order to understand
-              history and why people acted the way they did, one must also
-              understand the underlying narratives.
+              and emotions, how they spread like pathogens, thus influencing
+              society and the economy. In order to understand recessions, for
+              example, it is not enough to understand the cybernetic control
+              loops behind them, but one must also include the human factor with
+              it‘s irrational, emotionally driven actions. Above all, this
+              includes our belief in stories that create meaning, the
+              narratives.
             </p>
             <p>
-              These infectious narratives, behave similarly to pathogens and can
-              be simulated with the help of mathematical models. In this case,
-              the system dynamic behaviour is simulated by cellular automata.
+              Simulating the dynamic system behaviour of narratives helps us to
+              understand the underlying narratives of history, why people acted
+              the way they did, and thus history itself. These infectious
+              narratives, behave similarly to pathogens and can be simulated
+              with the help of mathematical models.
             </p>
             <p>
-              Each color represented here represents a narrative, the more
-              similar the colors the more similar the narrative. The narratives
-              can have a symbiotic effect with each other, can displace one
-              another and evolve.
+              In this case, the system dynamic behaviour is simulated by
+              cellular automata. By using cellular automata, in which the
+              individual cells are able to represent character values such as
+              political views, skepticism, liberalism, or existing knowledge
+              through mathematical modeling and can interact with other cells in
+              their immediate environment, it is possible to simulate this
+              complex system behavior in a simplified way and thus get a feeling
+              of how narratives spread in society. Narratives can emerge
+              spontaneously, evolve, be in competition, or symbiosis with other
+              narratives and can also be forgotten or dismissed.
             </p>
             <p>
-              The parameters you can select here allow you to adapt the company
-              structures. This way you can create a society that is totally
-              based on pluralism, allows itself to be polarized, consists only
-              of wild thinkers or is skeptical towards any kind of development.
+              This webapp was specifically developed to simulate the dynamic
+              system behavior of narratives in society using cellular automata
+              and mathematical modeling. The user can design different forms of
+              societies, such as pluralistic, populistic, or a society that is
+              sceptical towards any form of societal change. Each color
+              represented here represents a narrative, the more similar the
+              colors the more similar the narrative. The narratives can have a
+              symbiotic effect with each other, can displace one another, and
+              evolve. Through the complex interaction of narratives and their
+              unfolding, beautiful and fascinating landscapes emerge
+              simultaneously. Explore the dynamic spread and development of
+              narratives in different social systems.
             </p>
           </v-card-text>
           <v-card-actions>
